0dfa4a23dad70a09431d2e78ce912876201299a1
eevakoivu <eevamaria.koivu@gmail.com>
2022-03-16 15:46:40 +0200

    Initial commmit

---
+++ app/src/main/java/com/example/vk8_3/BottleDispenser.java
@@ -0,0 +1,60 @@
+package com.example.vk8_3;
+
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class BottleDispenser {
+    private int bottles;
+    private double money;
+
+    private static BottleDispenser bottledispenser = new BottleDispenser();
+
+    public static BottleDispenser getInstance(){
+        return bottledispenser;
+    }
+
+    private ArrayList<Bottle> list = new ArrayList<Bottle>();
+
+    private BottleDispenser() {
+        bottles = 5;
+        money = 0;
+
+        for(int i = 0;i<bottles;i++) {
+            list.add(new Bottle("Pepsi Max", 0.5, 1.8));
+        }
+
+    }
+
+    public void addMoney(TextView text) {
+        money += 1;
+        text.setText("Klink! Added more money!");
+    }
+
+
+    public void buyBottle(TextView text) {
+
+        if(money < list.get(bottles-1).getPrice()){ //indeksi 4
+            text.setText("Add money first!");
+        }
+        else if(bottles > 0){
+            bottles -= 1; //muuttuu indeksi 4
+            money -= list.get(bottles).getPrice(); //indeksi 4
+            text.setText("KACHUNK! " + list.get(bottles).getName() + " came out of the dispenser!"); //indeksi 4
+            removeObject();
+        }
+    }
+
+    public void returnMoney(TextView text) {
+
+        DecimalFormat formatted = new DecimalFormat("0.00");
+        text.setText("Klink klink. Money came out! You got " + formatted.format(money) + "€ back");
+        money = 0;
+    }
+
+    public void removeObject(){
+        list.remove(bottles);
+
+    }
+}
--- app/src/main/java/com/example/vk8_3/MainActivity.java
+++ app/src/main/java/com/example/vk8_3/MainActivity.java
@@ -2,13 +2,64 @@ package com.example.vk8_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context = null;
+    TextView text = null;
+    TextView text2 = null;
+    SeekBar seekbar = null;
+    int value = 0;
+
+    BottleDispenser bottledispenser = BottleDispenser.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = MainActivity.this;
+        text = (TextView) findViewById(R.id.textView);
+        text2 = (TextView) findViewById(R.id.textView2);
+        seekbar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                text2.setText(value);
+                value = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+    public void addMoney(View v){
+        bottledispenser.addMoney(text);
+    }
+
+    public void buyBottle(View v){
+        bottledispenser.buyBottle(text);
+    }
+
+    public void returnMoney(View v){
+        bottledispenser.returnMoney(text);
+    }
+
+        /*public void listBottles(View v){
+            bottledispenser.listBottles(text);
+        }*/
+
 }
